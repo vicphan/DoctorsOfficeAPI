02836ea62cf02b9fbff8ff4e7c80b25e3e690c62
Logan Boras <60116280+LoganBoras@users.noreply.github.com>
2020-12-06 14:02:34 -0700

    Create remove.php

---
+++ test/remove.php
@@ -0,0 +1,36 @@
+<?php
+//removes entry from database
+header("Access-Control-Allow-OriginL *");//allows all users access
+header("Content-Type: application/json; charset=UTF-8"); //sets content to json
+header("Access-Control-Allow-Methods: POST"); //modify database
+header("Access-Control-Max-Age: 300"); //results can be cached for 5 minutes
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once "../config/database.php"; //includes database.php file
+include_once "../entities/test.php"; //includes test.php file
+
+$database = new Database();
+$db = $database -> getConnection();
+
+$test = new Test($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (!empty($data->ID)){
+	$test ->id = $data -> ID;
+	if ($test -> remove()){
+		http_response_code(200);
+		echo json_encode(array("message" => "test successfully removed"));
+	}
+	else{
+		http_response_code(503);
+		echo json_encode(array("message" => "test was unable to be removed"));
+	}
+}
+else{
+	http_response_code(503);
+	echo json_encode(array("message" => "error"));
+}
+
+$db -> close();
+?>
